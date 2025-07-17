7e996ff9f9deae75b04d161d8c8e99452b5f42c5
Jon Allen <joallen@nvidia.com>
2025-07-17 16:32:10 +0000

    WIP

--- qwen-vl-finetune/qwenvl/data/__init__.py
+++ qwen-vl-finetune/qwenvl/data/__init__.py
@@ -1,37 +1,12 @@
 import re
 
-# Define placeholders for dataset paths
-CAMBRIAN_737K = {
-    "annotation_path": "PATH_TO_CAMBRIAN_737K_ANNOTATION",
-    "data_path": "",
-}
-
-CAMBRIAN_737K_PACK = {
-    "annotation_path": f"PATH_TO_CAMBRIAN_737K_ANNOTATION_PACKED",
-    "data_path": f"",
-}
-
-MP_DOC = {
-    "annotation_path": "PATH_TO_MP_DOC_ANNOTATION",
-    "data_path": "PATH_TO_MP_DOC_DATA",
-}
-
-CLEVR_MC = {
-    "annotation_path": "PATH_TO_CLEVR_MC_ANNOTATION",
-    "data_path": "PATH_TO_CLEVR_MC_DATA",
-}
-
-VIDEOCHATGPT = {
-    "annotation_path": "PATH_TO_VIDEOCHATGPT_ANNOTATION",
-    "data_path": "PATH_TO_VIDEOCHATGPT_DATA",
+VLAD_BENCH = {
+    "annotation_path": "/path/to/annotations.json",
+    "data_path": "/path/to/image/data",
 }
 
 data_dict = {
-    "cambrian_737k": CAMBRIAN_737K,
-    "cambrian_737k_pack": CAMBRIAN_737K_PACK,
-    "mp_doc": MP_DOC,
-    "clevr_mc": CLEVR_MC,
-    "videochatgpt": VIDEOCHATGPT,
+    "vlad_bench": VLAD_BENCH,
 }
 
 
@@ -54,10 +29,3 @@ def data_list(dataset_names):
         else:
             raise ValueError(f"do not find {dataset_name}")
     return config_list
-
-
-if __name__ == "__main__":
-    dataset_names = ["cambrian_737k"]
-    configs = data_list(dataset_names)
-    for config in configs:
-        print(config)
